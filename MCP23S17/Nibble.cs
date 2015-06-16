527983b0553470890ac99a080ebe843e9988728c
Alvin Stanescu <alvin.stanescu@gmail.com>
2015-06-16 02:12:01 +0200

    Initial commit

---
+++ MCP23S17/Nibble.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiFace
+{
+    /// <summary>
+    /// The nibble of a byte. 
+    /// The high nibble contains the bits from 7 to 4, the low nibble contains the bits from 3 to 0.
+    /// </summary>
+    internal enum Nibble
+    {
+        Lower = 0,
+        Upper = 1
+    }
+
+    /// <summary>
+    /// Extensions to ease the use of nibbles.
+    /// </summary>
+    internal static class NibbleExtensions
+    {
+        /// <summary>
+        /// Gets the range start from the nibble.
+        /// 0 (Nibble.Lower) => 0
+        /// 1 (Nibble.Upper) => 4
+        /// </summary>
+        /// <param name="nibble">The nibble.</param>
+        /// <returns>The beginning of the range, with a length of 4.</returns>
+
+        internal static int GetRangeStart(this Nibble nibble)
+        {
+            return (int)nibble << 2;
+        }
+
+        /// <summary>
+        /// Gets the range start from the nibble.
+        /// 0 (Nibble.Lower) => 4
+        /// 1 (Nibble.Upper) => 8
+        /// </summary>
+        /// <param name="nibble">The nibble.</param>
+        /// <returns>The beginning of the range, with a length of 4.</returns>
+
+        internal static int GetRangeEnd(this Nibble nibble)
+        {
+            return (int)(++nibble) << 2;
+        }
+
+        /// <summary>
+        /// Gets the shift left value needed to access the nibble.
+        /// </summary>
+        /// <param name="nibble">The nibble.</param>
+        /// <returns>The beginning of the range, with a length of 4.</returns>
+
+        internal static byte GetShiftValue(this Nibble nibble)
+        {
+            return (byte)(0x0F << ((byte)(nibble) << 4));
+        }
+
+        /// <summary>
+        /// Sets the part of a byte corresponding to the nibble and returns the byte.
+        /// </summary>
+        /// <param name="nibble">The nibble.</param>
+        /// <param name="value">The value of the nibble (4-bit).</param>
+        /// <param name="byteValue">The value of the byte on which to set the nibble.</param>
+        internal static void SetByteNibble(this Nibble nibble, byte value, ref byte byteValue)
+        {
+            var byteWithClearedNibble = (byte) (nibble == Nibble.Lower ? byteValue & 0xF0 : byteValue & 0x0F);
+            byteValue ^= (byte)(value & GetShiftValue(nibble));
+        }
+
+        /// <summary>
+        /// Sets the part of a negated byte corresponding to the nibble and returns the negated byte.
+        /// </summary>
+        /// <param name="nibble">The nibble.</param>
+        /// <param name="value">The value of the nibble (4-bit).</param>
+        /// <param name="byteValue">The value of the negated byte on which to set the nibble.</param>
+        internal static void SetByteNibbleNeg(this Nibble nibble, byte value, ref byte byteValue)
+        {
+            var byteWithClearedNibble = (byte)(nibble == Nibble.Lower ? byteValue & 0xF0 : byteValue & 0x0F);
+            byteValue ^= (byte)(value & GetShiftValue(nibble) ^ (0xF << ((int)nibble) << 2));
+        }
+    }
+}
---
+++ MCP23S17/Registers/MCP23S17RegisterBase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiFace
+{
+    /// <summary>
+    /// Base class for MCP23S17 register entities.
+    /// </summary>
+    public class MCP23S17RegisterBase
+    {
+        /// <summary>
+        /// The address of the entity.
+        /// </summary>
+        public byte Address { get; protected set; }
+
+        /// <summary>
+        /// The MCP23S17 chip which contains the register.
+        /// </summary>
+        protected readonly MCP23S17 _chip;
+
+        /// <summary>
+        /// Creates a new instance of the MCP23S17RegisterBase class.
+        /// </summary>
+        /// <param name="address">The address of the entity.</param>
+        /// <param name="chip">The MCP23S17 chip which contains the register.</param>
+        internal MCP23S17RegisterBase(byte address, MCP23S17 chip)
+        {
+            this.Address = address;
+            this._chip = chip;
+        }
+
+        /// <summary>
+        /// The value of the register entity.
+        /// </summary>
+        public virtual byte Value
+        {
+            get
+            {
+                return this._chip.Read(this.Address);
+            }
+            set
+            {
+                this._chip.Write(value, this.Address);
+            }
+        }
+
+        /// <summary>
+        /// Toggles all bits.
+        /// </summary>
+        public virtual void Toggle()
+        {
+            this.Value = (byte)(0xFF ^ this.Value);
+        }
+
+        /// <summary>
+        /// Sets all bits.
+        /// </summary>
+        public virtual void SetAllBits()
+        {
+            this.Value = 0xFF;
+        }
+
+        /// <summary>
+        /// Clears all bits.
+        /// </summary>
+        public virtual void ClearAllBits()
+        {
+            this.Value = 0x00;
+        }
+    }
+}
